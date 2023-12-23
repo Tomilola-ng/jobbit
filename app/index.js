f5661ca6c0de0e93e97ae3987e253e7bd222692c
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-12-23 16:21:35 +0000

    MO

--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { useRouter, Stack } from "expo-router";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+
+import { COLORS, SIZES, icons, images } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
 
 export default function Home() {
+  // useState
   return (
     <View>
       <Text> Home </Text>
