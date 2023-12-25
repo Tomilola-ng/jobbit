2c610679340d4c26addc0612f8002344f09a6b2a
Tomilola-ng <tee.o2809@gmail.com>
2023-12-25 23:46:14 +0100

    WELCOME

--- app/index.js
+++ app/index.js
@@ -3,14 +3,49 @@ import { useRouter, Stack } from "expo-router";
 import { Text, View, ScrollView, SafeAreaView } from "react-native";
 
 import { COLORS, SIZES, icons, images } from "../constants";
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 export default function Home() {
-  // useState
+  const router = useRouter();
+
   return (
-    <View>
-      <Text> Home </Text>
-      <Text> About </Text>
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.lightWhite,
+      }}
+    >
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
--- constants/index.js
+++ constants/index.js
@@ -3,3 +3,13 @@ import icons from "./icons";
 import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
 
 export { images, icons, COLORS, FONT, SIZES, SHADOWS };
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Freelance",
+  "Internship",
+  "Temporary",
+  "Contract",
+  "Commission",
+  "Volunteer",
+];
